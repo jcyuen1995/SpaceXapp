a673f562081669d28b2d86f56d4fbe93ec63d3d4
jcyuen1995 <jcyuen1995@gmail.com>
2021-02-14 00:07:14 -0500

    fixing heroku

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-
+// server.js
 const express = require('express');
 const app = express();
 // Run the app by serving the static files
@@ -7,3 +7,21 @@ app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
+
+const forceSSL = function() {
+    return function (req, res, next) {
+        if (req.headers['x-forwarded-proto'] !== 'https') {
+        return res.redirect(
+            ['https://', req.get('Host'), req.url].join('')
+            );
+        }
+        next();
+        }
+    }
+  // Instruct the app
+  // to use the forceSSL
+  // middleware
+    app.use(forceSSL());
+    app.get('/*', function(req, res) {
+        res.sendFile(path.join(__dirname + '/dist/index.html'));
+    });
