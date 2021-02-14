50a68ecbff112a695d05a500de0297ff69a66683
jcyuen1995 <jcyuen1995@gmail.com>
2021-02-14 01:07:35 -0500

    fixing heroku

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const app = express();
 // in the dist directory
 app.use(express.static(__dirname + '/dist/'));
 // Start the app by listening on the default
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
 // Heroku port
 app.listen(process.env.PORT || 8080);
 
