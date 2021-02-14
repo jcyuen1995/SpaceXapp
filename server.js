5bb42fafc0832a1a85da8fe01293520f6407aaaf
jcyuen1995 <jcyuen1995@gmail.com>
2021-02-14 01:17:20 -0500

    fixing heroku

--- server.js
+++ server.js
@@ -1,27 +1,16 @@
-// server.js
+//Install express server
 const express = require('express');
-const app = express();
-// Run the app by serving the static files
-// in the dist directory
-app.use(express.static(__dirname + '/dist/'));
-// Start the app by listening on the default
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
-// Heroku port
-app.listen(process.env.PORT || 8080);
+const path = require('path');
 
-const forceSSL = function() {
-    return function (req, res, next) {
-        if (req.headers['x-forwarded-proto'] !== 'https') {
-        return res.redirect(
-        ['https://', req.get('Host'), req.url].join('')
-        );
-    }
-    next();
-    }
-}
-// Instruct the app
-// to use the forceSSL
-// middleware
-app.use(forceSSL());
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/spacex'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/spacex/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
