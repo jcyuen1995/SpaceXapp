40d815843d70db340013553195275d3d8d29ce1b
jcyuen1995 <jcyuen1995@gmail.com>
2021-02-14 00:30:15 -0500

    fixing heroku

--- server.js
+++ server.js
@@ -12,16 +12,13 @@ const forceSSL = function() {
     return function (req, res, next) {
         if (req.headers['x-forwarded-proto'] !== 'https') {
         return res.redirect(
-            ['https://', req.get('Host'), req.url].join('')
-            );
-        }
-        next();
-        }
+        ['https://', req.get('Host'), req.url].join('')
+        );
     }
-  // Instruct the app
-  // to use the forceSSL
-  // middleware
-    app.use(forceSSL());
-    app.get('/*', function(req, res) {
-        res.sendFile(path.join(__dirname + '/dist/index.html'));
-    });
+    next();
+    }
+}
+// Instruct the app
+// to use the forceSSL
+// middleware
+app.use(forceSSL());
